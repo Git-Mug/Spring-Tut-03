c1af3b01c1b1c4bd580729118ba3a4acc7e66fdd
Sean <U1358754@unimail.hud.ac.uk>
2017-11-24 11:54:31 +0000

    Second Commit

---
+++ src/main/java/com/sean/guestbook/controller/GuestBookController.java
@@ -0,0 +1,14 @@
+package com.sean.guestbook.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GuestBookController {
+
+@GetMapping
+public String testMapping() {
+    return "Hello World";
+}
+
+}
---
+++ src/main/java/com/sean/guestbook/domain/GuestBookEntry.java
@@ -0,0 +1,4 @@
+package com.sean.guestbook.domain;
+
+public class GuestBookEntry {
+}
